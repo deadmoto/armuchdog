f9fecdd78d39dddd67b0847b83eecb39ed866630
deadmoto <deadmoto@e3a26972-0a0e-11de-a6e9-d320b29efae0>
2009-04-22 09:18:16 +0000

    preparing to deprecate summary report

--- trunk/forms/reportokved.pas
+++ trunk/forms/reportokved.pas
@@ -1,4 +1,4 @@
-unit ReportOKVED;
+unit reportokved;
 
 interface
 
@@ -9,7 +9,7 @@ uses
 procedure showreport;overload;
 procedure showreport(startd,endd:tdatetime;nomencl:string);overload;
 type
-  treport_okved = class(TForm)
+  tsizerrpt = class(TForm)
     nomenclbox: TGroupBox;
     regionbox: TGroupBox;
     nomencl: TComboBox;
@@ -29,23 +29,22 @@ type
     btnpanel: TPanel;
     exit: TButton;
     print: TButton;
-    procedure FormShow(Sender: TObject);
+    left: TButton;
     procedure nomenclKeyPress(Sender: TObject; var Key: Char);
-    procedure Button1Click(Sender: TObject);
     procedure gridSelectCell(Sender: TObject; ACol, ARow: Integer;
       var CanSelect: Boolean);
     procedure regioncbxChange(Sender: TObject);
     procedure nomenclChange(Sender: TObject);
-    procedure gridDblClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure quarterudChangingEx(Sender: TObject; var AllowChange: Boolean;
       NewValue: SmallInt; Direction: TUpDownDirection);
     procedure exitClick(Sender: TObject);
     procedure yearcbxChange(Sender: TObject);
     procedure quarteredChange(Sender: TObject);
+    procedure printClick(Sender: TObject);
+    procedure leftClick(Sender: TObject);
   private
     selected:integer;
-    procedure getregions;
     procedure fill;
   public
   end;
@@ -55,45 +54,32 @@ implementation
 uses
   datamodel,
   datamodule,
-  contractfm,
+  defs,
+  balance,
   regions,
   providers,
   util;
 
 {$R *.dfm}
 
-procedure treport_okved.getregions;
-var
-  i:integer;
-begin
-  regioncbx.items.clear;
-  regioncbx.items.add('*');
-  dm.query.sql.text:='SELECT * FROM RegionID';
-  dm.query.open;
-  dm.query.first;
-  for i:=0 to dm.query.recordcount-1 do
-    begin
-      regioncbx.items.add(trim(dm.query.fieldbyname('fldname').asstring));
-      dm.query.next;
-    end;
-  dm.query.close;
-end;
-
-procedure treport_okved.fill;
+procedure tsizerrpt.fill;
 var
   i:integer;
   price:real;
 begin
+  price:=0;
   with grid do
     begin
       cells[0,0]:=subcontract.okved.caption;
       cells[1,0]:=sizer.name.caption;
       cells[2,0]:=region.name.caption;
       cells[3,0]:=subcontract.price.caption;
+      cells[4,0]:='Остаток';
       colwidths[0]:=subcontract.okved.width;
       colwidths[2]:=region.name.width;
       colwidths[3]:=subcontract.price.width;
-      colwidths[1]:=width-(colwidths[0]+colwidths[2]+colwidths[3]+24);
+      colwidths[4]:=subcontract.price.width;
+      colwidths[1]:=width-(colwidths[0]+colwidths[2]+colwidths[3]+colwidths[4]+32);
     end;
   dm.query.sql.text:='SELECT '+commstr([subcontract.okved.name,sizer.name.name,
                                         region.name.name,sum(subcontract.price)])+#13+
@@ -131,6 +117,7 @@ begin
             grid.cells[1,i+1]:=fieldbyname(sizer.name.column).value;
             grid.cells[2,i+1]:=fieldbyname(region.name.column).value;
             grid.cells[3,i+1]:=fieldbyname(subcontract.price.column).value;
+            grid.cells[4,i+1]:='Неизвестно';
             price:=price+fieldbyname(subcontract.price.column).value;
             dm.query.next;
           end;
@@ -140,7 +127,7 @@ begin
   status.panels.items[3].text:=floattostr(price);
 end;
 
-procedure treport_okved.formcreate;
+procedure tsizerrpt.formcreate;
 var
   i:integer;
 begin
@@ -165,26 +152,15 @@ begin
   dm.query.first;
   for i:=0 to dm.query.recordcount-1 do
     begin
-      nomencl.items.add(trim(dm.query.fieldbyname(subcontract.okved.column).asstring));
+      nomencl.items.add(trim(dm.query.fieldbyname(subcontract.okved.column).value));
       dm.query.next;
     end;
+  regioncbx.items.add('*');
+  for i:=0 to length(cregion)-1 do
+    regioncbx.items.add(cregion[i].name);
   nomencl.items.strings[0]:='*';
   yearcbx.text:=formatdatetime('yyyy',now);
   quarterud.position:=quarter(now);
-
-end;
-
-procedure treport_okved.FormShow(Sender: TObject);
-var
-  month:word;
-  temp:word;
-begin
-  decodedate(now,temp,month,temp);
-  endpick.date:=encodedate(currentyear,month,1);
-  startpick.datetime:=incmonth(endpick.date,-3);
-  getregions;
-  nomencl.itemindex:=0;
-  regioncbx.itemindex:=0;
   fill;
 end;
 
@@ -192,13 +168,12 @@ procedure showreport;
 var
   month:word;
   temp:word;
-  report:treport_okved;
+  report:tsizerrpt;
 begin
-  report:=treport_okved.create(application.owner);
+  report:=tsizerrpt.create(application.owner);
   decodedate(now,temp,month,temp);
   report.endpick.date:=encodedate(currentyear,month,1);
   report.startpick.datetime:=incmonth(report.endpick.date,-3);
-  report.getregions;
   report.nomencl.itemindex:=0;
   report.regioncbx.itemindex:=0;
 
@@ -210,74 +185,78 @@ end;
 procedure showreport(startd,endd:tdatetime;nomencl:string);
 var
   i:integer;
-  report:treport_okved;
+  report:tsizerrpt;
 begin
-  report:=treport_okved.create(application.owner);
+  report:=tsizerrpt.create(application.owner);
   report.show;
   report.startpick.date:=startd;
   report.endpick.date:=endd;
-  report.getregions;
   for i:=0 to report.nomencl.items.count-1 do
     if report.nomencl.items.strings[i]=nomencl then
       report.nomencl.itemindex:=i;
-//  report.search.click;
 end;
 
-procedure treport_okved.nomenclKeyPress(Sender: TObject; var Key: Char);
-begin
-  key:=chr(vk_cancel);
-end;
-
-procedure treport_okved.Button1Click(Sender: TObject);
+procedure tsizerrpt.printclick;
 begin
   if nomencl.itemindex=0 then
     dm.report.loadfromfile(extractfilepath(paramstr(0))+'reports\okved_by_pbs.fr3')
   else
     dm.report.loadfromfile(extractfilepath(paramstr(0))+'reports\okved_by_okved.fr3');
-  dm.report.variables.variables['start']:=startpick.datetime;
-  dm.report.variables.variables['finish']:=endpick.datetime;
+  dm.report.variables.variables['year']:=yearcbx.text;
+  dm.report.variables.variables['quarter']:=quartered.text;
   dm.report.showreport;
 end;
 
-procedure treport_okved.gridDblClick;
-begin
-//  if dm.query.recordcount>0 then
-//    contractform.edit(strtoint(grid.cells[0,selected]));
-end;
-
-procedure treport_okved.gridselectcell;
+procedure tsizerrpt.gridselectcell;
 begin
   if arow>0 then
     selected:=arow;
 end;
 
-procedure treport_okved.quarterudchangingex;
+procedure tsizerrpt.leftclick;
+var
+  i:integer;
+begin
+  for i:=1 to grid.rowcount-1 do
+    begin
+      grid.cells[4,i]:=floattostr(contractlimit-getbalance(0,grid.cells[0,i],
+                       strtoint(yearcbx.text),strtoint(quartered.text)));
+      refresh;
+    end;
+end;
+
+procedure tsizerrpt.quarterudchangingex;
 begin
   if newvalue in [1..4] then
     quartered.text:=inttostr(newvalue);
 end;
 
-procedure treport_okved.yearcbxchange;
+procedure tsizerrpt.nomenclKeyPress(Sender: TObject; var Key: Char);
+begin
+  key:=chr(vk_cancel);
+end;
+
+procedure tsizerrpt.yearcbxchange;
 begin
   fill;
 end;
 
-procedure treport_okved.quarteredchange;
+procedure tsizerrpt.quarteredchange;
 begin
   fill;
 end;
 
-procedure treport_okved.regioncbxChange;
+procedure tsizerrpt.regioncbxChange;
 begin
   fill;
 end;
 
-procedure treport_okved.nomenclChange;
+procedure tsizerrpt.nomenclChange;
 begin
   fill;
 end;
 
-procedure treport_okved.exitclick;
+procedure tsizerrpt.exitclick;
 begin
   close;
 end;
