9569e68d7860000a15fe382636deae9ec064785c
deadmoto <deadmoto@e3a26972-0a0e-11de-a6e9-d320b29efae0>
2009-11-18 10:54:39 +0000

    added Provider struct.

--- trunk/Tables.cs
+++ trunk/Tables.cs
@@ -7,6 +7,7 @@ namespace Contracts.NET
     {
         public List<SQLContract> Contracts;
         public List<SQLRegion> Regions;
+        public List<SQLProvider> Providers;
 
         private SQLRegion FindRegion(short Id)
         {
@@ -18,6 +19,18 @@ namespace Contracts.NET
                 }
             }
             return new SQLRegion();
+        }
+
+        private SQLProvider FindProvider(int Id)
+        {
+            foreach (SQLProvider Provider in Providers)
+            {
+                if (Provider.Id == Id)
+                {
+                    return Provider;
+                }
+            }
+            return new SQLProvider();
         }
 
         public void GetRegions()
@@ -34,6 +47,20 @@ namespace Contracts.NET
             Reader.Close();
         }
 
+        public void GetProviders()
+        {
+            Providers = new List<SQLProvider>();
+            SqlDataReader Reader = SQLServer.SQLExecute("SELECT * FROM SupplierDog");
+            while (Reader.Read())
+            {
+                SQLProvider Provider = new SQLProvider();
+                Provider.Id = Reader.GetInt32(Reader.GetOrdinal("ID_SUPPLIER"));
+                Provider.Name = Reader.GetString(Reader.GetOrdinal("SUPPLIER")).Trim();
+                Providers.Add(Provider);
+            }
+            Reader.Close();
+        }
+
         public void GetContracts()
         {
             Contracts = new List<SQLContract>();
@@ -41,18 +68,22 @@ namespace Contracts.NET
             while (Reader.Read())
             {
                 SQLContract Contract = new SQLContract();
-                Contract.REGN = Reader.GetInt64(Reader.GetOrdinal("REGN"));
-                Contract.registration = Reader.GetString(Reader.GetOrdinal("registration"));
-                Contract.N_DOG = Reader.GetString(Reader.GetOrdinal("N_DOG"));
+                Contract.Id = Reader.GetInt64(Reader.GetOrdinal("REGN"));
+                Contract.RegNum = Reader.GetString(Reader.GetOrdinal("registration"));
+                Contract.ConNum = Reader.GetString(Reader.GetOrdinal("N_DOG"));
                 if (!Reader.IsDBNull(Reader.GetOrdinal("DATA_REG")))
                 {
-                    Contract.DATA_REG = Reader.GetDateTime(Reader.GetOrdinal("DATA_REG"));
+                    Contract.RegDate = Reader.GetDateTime(Reader.GetOrdinal("DATA_REG"));
                 }
                 if (!Reader.IsDBNull(Reader.GetOrdinal("DATA_POST")))
                 {
                     Contract.DATA_POST = Reader.GetDateTime(Reader.GetOrdinal("DATA_POST"));
                 }
                 Contract.Region = FindRegion(Reader.GetByte(Reader.GetOrdinal("FLDID")));
+                if (!Reader.IsDBNull(Reader.GetOrdinal("ID_SUPPLIER")))
+                {
+                    Contract.Provider = FindProvider(Reader.GetInt32(Reader.GetOrdinal("ID_SUPPLIER")));
+                }
                 Contracts.Add(Contract);
             }
             Reader.Close();
@@ -61,6 +92,7 @@ namespace Contracts.NET
         public SQLTables()
         {
             GetRegions();
+            GetProviders();
             GetContracts();
         }
     }   
