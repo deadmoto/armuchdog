b9e6227eb610987106f1f5c1a9122cfc8884cd0f
deadmoto <deadmoto@e3a26972-0a0e-11de-a6e9-d320b29efae0>
2010-04-21 07:48:33 +0000

    momoved NCEA class to Classifier class added ToArray() method

---
+++ Core/Classifier.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Contracts.NET
+{
+    /// <summary>
+    /// Represents classifier data
+    /// </summary>
+    public struct ClassifierData
+    {
+        public string Id;
+        public string Name;
+
+        /// <summary>
+        /// Returns array of fields
+        /// </summary>
+        public object[] ToArray()
+        {
+            object[] Result = new object[2];
+            Result[0] = Id;
+            Result[1] = Name;
+            return Result;
+        }
+    }
+
+    /// <summary>
+    /// Provides classifier list
+    /// </summary>
+    static class Classifier
+    {
+        public static List<ClassifierData> ClassifierList = new List<ClassifierData>();
+
+        /// <summary>
+        /// Returns classifier by Id
+        /// </summary>
+        public static ClassifierData Find(string Id)
+        {
+            return ClassifierList.Find(delegate(ClassifierData Item) { return Item.Id == Id; });
+        }
+
+        /// <summary>
+        /// Retrieves classifier list from database
+        /// </summary>
+        public static void Retrieve()
+        {
+            ClassifierList.Clear();
+            SqlCommand Command = Connection.GetCommand("SELECT * FROM NomenclDog");
+            SqlDataReader Reader = Command.ExecuteReader();
+            while (Reader.Read())
+            {
+                ClassifierData Item = new ClassifierData();
+                Item.Id = Reader.GetString(Reader.GetOrdinal("id_nomencl")).Trim();
+                Item.Name = Reader.GetString(Reader.GetOrdinal("NAME")).Trim();
+                ClassifierList.Add(Item);
+            }
+        }
+    }
+}
